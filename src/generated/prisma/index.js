02809bbaabcff09914b18c4a1b3f65a5958476ae
bookstoremanagex <bookstoremanagex@gmail.com>
2026-05-23 09:45:50 +0300

    update: fix

--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -849,7 +849,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
